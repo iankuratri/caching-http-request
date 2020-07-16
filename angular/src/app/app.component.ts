a7b3877c325806834b366dd14a806f85065659fb
Ankur Atri <ankuratri.in@gmail.com>
2020-07-16 19:31:31 +0530

    trying different ways for caching

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subscription, interval } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  private todo$: Observable<any>;
+  private todoSub: Subscription;
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchTodo();
+
+    this.todoSub = this.todo$.subscribe((todo) => {
+      console.log('todo from sub:', todo);
+    });
+  }
+
+  // fetchTodo() {
+  //   this._http
+  //     .get('https://jsonplaceholder.typicode.com/todos/1')
+  //     .subscribe((res: any) => {
+  //       console.log('response from API', res);
+  //     });
+  // }
+
+  fetchTodo() {
+    console.log('fetchTodo called');
+
+    if (!this.todo$) {
+      this.todo$ = this._http
+        .get('https://jsonplaceholder.typicode.com/todos/1')
+        .pipe(shareReplay(1));
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.todoSub) this.todoSub.unsubscribe();
+  }
 }
